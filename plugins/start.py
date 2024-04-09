8527538d3b11469938f33e02b3de71ba6af2ed1a
Mrblackgodd <136906179+Mrblackgodd@users.noreply.github.com>
2024-04-09 19:05:36 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -21,7 +21,7 @@ async def start_message(c,m):
     await db.is_user_exist(m.from_user.id)
     await db.add_user(m.from_user.id, m.from_user.first_name)
     await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
-    await m.react(emoji=random.choice(REACTIONS))
+    await m.react(REACTIONS)
     await m.reply_photo(f"https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
         caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[𝐏𝐔𝐑𝐏𝐋𝐄 𝐁𝐎𝐓𝐙](https://t.me/purpleebots)**",
         reply_markup=InlineKeyboardMarkup(
