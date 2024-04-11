71ab58907ac154123df156d2a7b24567a5b15c23
Mrblackgodd <136906179+Mrblackgodd@users.noreply.github.com>
2024-04-11 15:56:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,32 +8,6 @@ from pyrogram.types import (
     Message
 )
 
-import telegram
-from telegram.ext import Updater, CommandHandler
-from telegram import InlineKeyboardMarkup, InlineKeyboardButton
-
-# Replace 'YOUR_BOT_TOKEN' with your actual bot token
-TOKEN = 'BOT_TOKEN'
-
-# Replace 'YOUR_CHANNEL_ID' with your channel ID
-CHANNEL_ID = 'purplebotz'
-
-def start(update, context):
-    keyboard = [[InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{CHANNEL_ID}")]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text("To receive updates, please join our channel:", reply_markup=reply_markup)
-
-def main():
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler('start', start))
-
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
 
 LOG_TEXT = """<b>#NewUsergpt
     
@@ -44,21 +18,29 @@ Nᴀᴍᴇ - {}</b>
 
 @Client.on_message(filters.command('start'))
 async def start_message(c,m):
+    updates_channel = "https://t.me/purplebotz"
+
+    # Check if the user has joined the updates channel
+    if not await c.get_chat_member(updates_channel, m.from_user.id):
+        await m.reply_text("Please join our updates channel to use this bot.")
+        return
+
     await db.is_user_exist(m.from_user.id)
     await db.add_user(m.from_user.id, m.from_user.first_name)
     await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
-    await m.reply_photo(f"https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
-        caption="**ʜɪ** 👋\n\n**ɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ**\n\n⭕ **ᴘᴏᴡᴇʀᴇᴅ ʙʏ :-** **[𝐏𝐔𝐑𝐏𝐋𝐄 𝐁𝐎𝐓𝐙](https://t.me/purpleebots)**",
+    
+    # Send the welcome message
+    await m.reply_photo("https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
+        caption="ʜɪ 👋\n\nɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ\n\n⭕ ᴘᴏᴡᴇʀᴇᴅ ʙʏ :- [PURPLEBOTZ](https://t.me/purplebotz)",
         reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('🎥Movies/Webseries Search Group💕', url='https://t.me/+ylvI8ZZcge80MWRl')
+                        InlineKeyboardButton('💝 Movies', url='https://t.me/+ylvI8ZZcge80MWRl')
                     ],  
                     [
-                        InlineKeyboardButton("🥊𝐌𝐎𝐑𝐄 𝐁𝐎𝐓𝐒🥊", url='https://t.me/ezpzsupport/17'),
-                        InlineKeyboardButton("😍Anime😍", url='https://t.me/+nKz9rQJ893BlMGRl')
+                        InlineKeyboardButton("❣️ ADMIN", url='https://t.me/Lordsakunaa'),
+                        InlineKeyboardButton("🤖 MORE BOTS", url='https://t.me/purplebotz')
                     ]
                 ]
             )
-        )
-  
+    )
