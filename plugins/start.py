c5b6cd10b338269d932b5d1629a3ce9953a4ef9c
Mrblackgodd <136906179+Mrblackgodd@users.noreply.github.com>
2024-04-11 20:08:54 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,6 @@
+
 from pyrogram import Client, filters
-from config import LOG_CHANNEL, REACTIONS
+from config import LOG_CHANNEL
 from plugins.database import db
 from pyrogram.types import (
     InlineKeyboardMarkup,
@@ -8,39 +9,43 @@ from pyrogram.types import (
     Message
 )
 
+LOG_TEXT = """#NewUser
 
-LOG_TEXT = """<b>#NewUsergpt
-    
-ID - <code>{}</code>
+ID - {}
 
-Nᴀᴍᴇ - {}</b>
+Nᴀᴍᴇ - {}
 """
 
+UPDATES_CHANNEL = '@purplebotz'
+
 @Client.on_message(filters.command('start'))
-async def start_message(c,m):
-    updates_channel = "@purplebotz"
+async def start_message(c, m):
+    user_id = m.from_user.id
 
-    # Check if the user has joined the updates channel
-    if not await c.get_chat_member(updates_channel, m.from_user.id):
-        await m.reply_text("Please join our updates channel to use this bot.")
-        return
+    # Check if the user is already in the database
+    user_exists = await db.is_user_exist(user_id)
 
-    await db.is_user_exist(m.from_user.id)
-    await db.add_user(m.from_user.id, m.from_user.first_name)
-    await c.send_message(LOG_CHANNEL, LOG_TEXT.format(m.from_user.id, m.from_user.mention))
-    
-    # Send the welcome message
-    await m.reply_photo("https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
-        caption="ʜɪ 👋\n\nɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ\n\n⭕ ᴘᴏᴡᴇʀᴇᴅ ʙʏ :- [PURPLEBOTZ](https://t.me/purplebotz)",
-        reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton('💝 Movies', url='https://t.me/+ylvI8ZZcge80MWRl')
-                    ],  
-                    [
-                        InlineKeyboardButton("❣️ ADMIN", url='https://t.me/Lordsakunaa'),
-                        InlineKeyboardButton("🤖 MORE BOTS", url='https://t.me/purplebotz')
-                    ]
-                ]
-            )
-    )
+    if not user_exists:
+        # Check if the user has joined the updates channel
+        if await c.get_chat_member(UPDATES_CHANNEL, user_id):
+            await db.add_user(user_id, m.from_user.first_name)
+            await c.send_message(LOG_CHANNEL, LOG_TEXT.format(user_id, m.from_user.mention))
+            await m.reply_photo("https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
+                               caption="ʜɪ 👋\n\nɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ\n\n⭕ ᴘᴏᴡᴇʀᴇᴅ ʙʏ :- Tᴇᴄʜ VJ",
+                               reply_markup=InlineKeyboardMarkup(
+                                   [
+                                       [
+                                           InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ',
+                                                               url='https://youtube.com/@Tech_VJ')
+                                       ],
+                                       [
+                                           InlineKeyboardButton("❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/Kingvj01'),
+                                           InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url='https://t.me/VJ_Botz')
+                                       ]
+                                   ]
+                               )
+                               )
+        else:
+            await m.reply_text("Please join our updates channel to use this bot.")
+    else:
+        await m.reply_text("You have already started using the bot.")
