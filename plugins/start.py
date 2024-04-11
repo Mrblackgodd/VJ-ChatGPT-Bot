948ad1de941518b28853343fe256ee73bdd7c5c4
Mrblackgodd <136906179+Mrblackgodd@users.noreply.github.com>
2024-04-11 20:27:16 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -16,36 +16,33 @@ ID - {}
 Nᴀᴍᴇ - {}
 """
 
-UPDATES_CHANNEL = '@purplebotz'
-
 @Client.on_message(filters.command('start'))
 async def start_message(c, m):
     user_id = m.from_user.id
+    user_firstname = m.from_user.first_name
+    channel_id = -1001967167299  # Update this with your channel ID
 
-    # Check if the user is already in the database
-    user_exists = await db.is_user_exist(user_id)
+    # Check if the user is already in the channel
+    is_member = await c.get_chat_member(channel_id, user_id)
+    if is_member.status == 'left':
+        await m.reply_text("Please join the updates channel first to use this bot.\n@purplebotz")
+        return
 
-    if not user_exists:
-        # Check if the user has joined the updates channel
-        if await c.get_chat_member(UPDATES_CHANNEL, user_id):
-            await db.add_user(user_id, m.from_user.first_name)
-            await c.send_message(LOG_CHANNEL, LOG_TEXT.format(user_id, m.from_user.mention))
-            await m.reply_photo("https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
-                               caption="ʜɪ 👋\n\nɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ\n\n⭕ ᴘᴏᴡᴇʀᴇᴅ ʙʏ :- Tᴇᴄʜ VJ",
-                               reply_markup=InlineKeyboardMarkup(
-                                   [
-                                       [
-                                           InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ',
-                                                               url='https://youtube.com/@Tech_VJ')
-                                       ],
-                                       [
-                                           InlineKeyboardButton("❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/Kingvj01'),
-                                           InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url='https://t.me/VJ_Botz')
-                                       ]
-                                   ]
-                               )
-                               )
-        else:
-            await m.reply_text("Please join our updates channel to use this bot.")
-    else:
-        await m.reply_text("You have already started using the bot.")
+    await db.is_user_exist(user_id)
+    await db.add_user(user_id, user_firstname)
+    await c.send_message(LOG_CHANNEL, LOG_TEXT.format(user_id, m.from_user.mention))
+    await m.reply_photo(
+        "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg",
+        caption="ʜɪ 👋\n\nɪ ᴀᴍ ᴀ ᴄʜᴀᴛɢᴘᴛ ʙᴏᴛ\n\n⭕ ᴘᴏᴡᴇʀᴇᴅ ʙʏ :- Tᴇᴄʜ VJ",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton('💝 sᴜʙsᴄʀɪʙᴇ ʏᴏᴜᴛᴜʙᴇ ᴄʜᴀɴɴᴇʟ', url='https://youtube.com/@Tech_VJ')
+                ],
+                [
+                    InlineKeyboardButton("❣️ ᴅᴇᴠᴇʟᴏᴘᴇʀ", url='https://t.me/Kingvj01'),
+                    InlineKeyboardButton("🤖 ᴜᴘᴅᴀᴛᴇ", url='https://t.me/VJ_Botz')
+                ]
+            ]
+        )
+    )
